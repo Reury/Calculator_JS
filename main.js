90d9e7cc4370381b8f26b3795001d99b81ffb227
Reury <reuryaraujo@gmail.com>
2022-05-29 19:54:38 -0300

    fisrt commit full functional basic calculator with html, css, javascript

--- main.js
+++ main.js
@@ -1,38 +1,79 @@
 //console.log("works")
 class Calculator{
+
+
+
+
     constructor(previousOperandTextElement,currentOperandTextElement){
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currentOperandTextElement
+        this.previousOperandTextElement = this.previousOperandTextElement
+        this.currentOperandTextElement = this.currentOperandTextElement
         this.clear();
     }
 
     clear(){
-        this.currentOperandTextElement = ''
-        this.previousOperandTextElement = ''
+        this.currentOperand = ''
+        this.previousOperand = ''
         this.operation = undefined
 
     }
 
 
     delete(){
+        this.currentOperand = this.currentOperand.slice(0,this.currentOperand.length -1)
         
     }
 
     appendNumber(number){
-        this.currentOperand = number
+        if(number === '.' && this.currentOperand.includes('.')) return
+        //console.log("entrou o numero : "+ number)
+        this.currentOperand = currentOperandTextElement.innerText + number.toString()
     }
 
     chooseOperation(operation){
+        if(this.currentOperand === '' ) return
+        if(this.previousOperand !== ''){
+            this.compute()
+        }
+        this.operation = operation
+        /*console.log(operation)
+        console.log(this.currentOperand)
+        */
+        this.previousOperand = currentOperandTextElement.innerText + operation
+        this.currentOperand = ''
 
     }
 
 
     compute(){
+        this.previousOperand =previousOperandTextElement.innerText +this.currentOperand
+        switch(this.operation){
+            case '+':
+                this.currentOperand = parseFloat(this.previousOperand)  
+                + parseFloat(this.currentOperand);
+                break;
+            case '-':
+                this.currentOperand = parseFloat(this.previousOperand)  
+                - parseFloat(this.currentOperand);
+                break;
+            case '*':
+                this.currentOperand = parseFloat(this.previousOperand)  
+                * parseFloat(this.currentOperand);
+                break;
+             case '/':
+                    this.currentOperand = parseFloat(this.previousOperand)  
+                    / parseFloat(this.currentOperand);
+                    this.currentOperand = parseFloat(this.currentOperand).toFixed(2)
+                    break;
+    
+        }
+            
+        
 
     }
 
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
+        currentOperandTextElement.innerText = this.currentOperand
+        previousOperandTextElement.innerText = this.previousOperand
     }
 
 
@@ -41,7 +82,7 @@ class Calculator{
 
 
 
-const numberButtons = document.querySelectorAll('data-number')
+const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
@@ -49,15 +90,50 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
-
 const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
 
+/*previousOperandTextElement.addEventListener('click',()=>{
+    console.log("teste pra ver se area ta pegando correto")
+})*/
 
-numberButtons.forEach(button => {
-    button.addEventListener('click', ()=>{
-       calculator.appendNumber(button.innerText) 
-       console.log("apertou : " + button)
-       calculator.updateDisplay()
+
+numberButtons.forEach(numberButtons => {
+    numberButtons.addEventListener('click', ()=>{
+        //console.log("clickou") works til here
+        calculator.appendNumber(numberButtons.innerText) 
+        //console.log("apertou : " + numberButtons.innerText)
+        calculator.updateDisplay()
     })
     
 });
+
+operationButtons.forEach(operationButtons => {
+    operationButtons.addEventListener('click', ()=>{
+        //console.log("clickou") works til here
+        //console.log(operationButtons.innerText)
+        calculator.chooseOperation(operationButtons.innerText) 
+        calculator.updateDisplay()
+    })
+    
+});
+
+
+//console.log(deleteButton.innerText)
+
+deleteButton.addEventListener('click', ()=>{
+    //console.log("clickou para apagar")
+    calculator.delete()
+    calculator.updateDisplay()
+})
+
+
+allClearButton.addEventListener('click', ()=>{
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+
+equalsButton.addEventListener('click', () =>{
+    calculator.compute()
+    calculator.updateDisplay()
+})
